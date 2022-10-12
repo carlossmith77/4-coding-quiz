e87ac9645e6a68b419334547af9f96579bd9fd7c
carlossmith77 <carlosking12377Gmail.com>
2022-10-12 13:58:23 -0500

    added quiz questions and answers and button css

---
+++ 02-Challenge/Assets/js/script.js
@@ -0,0 +1,39 @@
+
+const timer = document.querySelector('#timer');
+const mainBox = document.querySelector('#main-box');
+const startButton = document.querySelector('#start-button')
+const answerButtons = document.querySelector('.custom-button')
+
+
+const questions = [
+  {
+    id: 0,
+    question: 'commonly used data types do not include?: ',
+    answers: ['<strings>','<booleans>','<alerts>','<numbers>'],
+    correctIndex: 2
+  },
+  {
+    id: 1,
+    question: 'The condition in an if/else statement is enclosed within?',
+    answers: ['<Quotes>','<curly brackets>','<parenthases>','<square brackets>'],
+    correctIndex: 2
+  },
+  {
+    id: 2,
+    question: 'Arrays in javascript can used to store _____',
+    answers: ['<Numbers and strings>','<other arrays>','<booleans>','<all of the above>'],
+    correctIndex: 3
+  },
+  {
+    id: 3,
+    question: 'string values must be enclosed within ______ when being assigned to variables "?',
+    answers: ['<commas>','<curly brackets>','<qoutes>','<parentheses>'],
+    correctIndex: 2
+  },
+  {
+    id: 4,
+    question: 'a very useful tool used during development and debugging for printing content to the debugger is',
+    answers: ['<javascript>','<terminal/bash>','<for loops>','<console log>'],
+    correctIndex: 3
+  }
+]
