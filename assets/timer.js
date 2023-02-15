f9c3f2f04ae09ca7be2830a6fe34caa4f19d3000
carlossmith77 <carlosking12377@gmail.com>
2023-02-14 19:23:39 -0600

    js tweaks

--- assets/timer.js
+++ assets/timer.js
@@ -1,35 +1,34 @@
 class Timer {
-    constructor(startValue,target) {
-      this.startValue = startValue;
-      this.currentVal = startValue;
-      this.targetElement = target;
-      this.timerID;
-    }
-
-    toString(timerVal = this.currentVal) {
-      if (timerVal >= 0) {
-        return new Date(timerVal * 500).toISOString().substr(14, 5)
-      } else {
-        return '00:00'
-      }
-    }
-
-    startTimer(interval = 1000) {
-        this.targetElement.innerText = this.toString();
-      this.timerID = setInterval(() => {
-        this.currentVal -= 1;
-        let stringTime = this.toString();
-        this.targetElement.innerText = stringTime;
-
-        if (this.currentVal <= 0) {
-          this.stopTimer();
-          quiz1.resultsPageGenerator();
-        }
-      }, interval);
-    }
-
-    stopTimer() {
-      clearInterval(this.timerID)
-    }
-  
+  constructor(startValue, targetElement) {
+    this.startValue = startValue;
+    this.currentVal = startValue;
+    this.targetElement = targetElement;
+    this.timerID = null;
   }
+
+  toString(timerVal = this.currentVal) {
+    if (timerVal >= 0) {
+      return new Date(timerVal * 500).toISOString().substr(14, 5);
+    } else {
+      return '00:00';
+    }
+  }
+
+  startTimer(interval = 1000) {
+    this.targetElement.innerText = this.toString();
+    this.timerID = setInterval(() => {
+      this.currentVal -= 1;
+      let stringTime = this.toString();
+      this.targetElement.innerText = stringTime;
+
+      if (this.currentVal <= 0) {
+        this.stopTimer();
+        quiz1.resultsPageGenerator();
+      }
+    }, interval);
+  }
+
+  stopTimer() {
+    clearInterval(this.timerID);
+  }
+}
